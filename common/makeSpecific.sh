6a8251e51034ed6d6f5434a6688650bd323b857b
pambros <pambros@users.noreply.github.com>
2017-12-01 15:37:46 +0100

    shell in progress

---
+++ common/makeSpecific.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+@set _OLD_PATH=%PATH%
+
+@set _LIB_PATH=%1
+@set _LIB_NAME=%2
+@echo %_LIB_NAME%
+@cd %_CURRENT_PATH%/%_LIB_PATH%/%_LIB_NAME%
+
+@set _GENERATED_PATH=generated
+@if not exist %_GENERATED_PATH% (
+	@mkdir "%_GENERATED_PATH%"
+)
+
+@call make_%_LIB_NAME%.bat -nopause > %_GENERATED_PATH%/stdoutmake_%_LIB_NAME%.%_USER_SPECIFIC%.txt 2> %_GENERATED_PATH%/stderrmake_%_LIB_NAME%.%_USER_SPECIFIC%.txt
+
+@set PATH=%_OLD_PATH%
