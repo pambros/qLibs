ff8524c5901228b2faf3cc705473f2856ea7ea83
pambros <pambros@users.noreply.github.com>
2017-12-08 11:35:48 +0100

    shell fix

--- makeAll.sh
+++ makeAll.sh
@@ -3,12 +3,12 @@ source ./localProjectPath.sh
 source $_PROJECT_PATH/globalVariables.sh
 _CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# source $_PROJECT_PATH/common/makeSpecific.sh thirdParties nasm
-# source $_PROJECT_PATH/common/makeSpecific.sh thirdParties zlib
+source $_PROJECT_PATH/common/makeSpecific.sh thirdParties nasm
+source $_PROJECT_PATH/common/makeSpecific.sh thirdParties zlib
 # source $_PROJECT_PATH/common/makeSpecific.sh thirdParties python
-# source $_PROJECT_PATH/common/makeSpecific.sh thirdParties boost
-# source $_PROJECT_PATH/common/makeSpecific.sh thirdParties gdcm
-# source $_PROJECT_PATH/common/makeSpecific.sh thirdParties itk
+source $_PROJECT_PATH/common/makeSpecific.sh thirdParties boost
+source $_PROJECT_PATH/common/makeSpecific.sh thirdParties gdcm
+source $_PROJECT_PATH/common/makeSpecific.sh thirdParties itk
 source $_PROJECT_PATH/common/makeSpecific.sh thirdParties vtk
 
 if [ "$1" != "-nopause" ]; then
